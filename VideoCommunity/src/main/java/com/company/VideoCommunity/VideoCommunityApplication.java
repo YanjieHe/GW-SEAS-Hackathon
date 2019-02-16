6e86ffde92ff34f6b42fa735bdf402b767b96c12
YanjieHe <heyanjie0@outlook.com>
2019-02-16 16:25:28 -0500

    video community

---
+++ VideoCommunity/src/main/java/com/company/VideoCommunity/VideoCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.company.VideoCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCommunityApplication.class, args);
+	}
+
+}
